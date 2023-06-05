472994b7de4868c64dd61dee8a192fdeadd3091d
Siavash Kordi Darian <sdaryan@gmail.com>
2023-06-05 23:06:41 +0430

    [200~The errors have been fixed.

--- Laptop Prices.py
+++ Laptop Prices.py
@@ -1,17 +1,22 @@
 list1=[]
 list2=[]
 list3=[]
+happy=0
+poor=0
 n=int(input())
 for k in range(0,n):
     list1= input().split()
     list2.append(list1)
     
 list3 = [[int(item) for item in sublist] for sublist in list2]
-
-for i in range(len(list2)-1):
-    if list2[i][i] < list2[i+1][i] and list2[i][i+1] > list2[i+1][i+1]:
-        print("happy irsa")
-        break
-    else :   
-         print("poor irsa")
-         break
+i=0
+for i in range(len(list3)):
+    for j in range(i+1, len(list3)):
+        if list3[i][0] < list3[j-i][0] and list3[i][1] > list3[j-i][1]:
+            happy=happy+1
+              
+    
+if happy >  poor :
+    print  ('happy irsa')
+else:
+    print  ('poor irsa')
